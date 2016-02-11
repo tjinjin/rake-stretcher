65d38f1338a80b3c419d9c927b0de07b119ddf90
tjinjin <tjinjinprogram@gmail.com>
2016-02-11 18:29:06 +0900

    Initial commit

---
+++ lib/rake/stretcher.rb
@@ -0,0 +1,7 @@
+require "rake/stretcher/version"
+
+module Rake
+  module Stretcher
+    # Your code goes here...
+  end
+end
---
+++ spec/rake/stretcher_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Rake::Stretcher do
+  it 'has a version number' do
+    expect(Rake::Stretcher::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
